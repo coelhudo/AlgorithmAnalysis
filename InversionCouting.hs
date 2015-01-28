1fc9dbbdd7de70a4b102d3d75b547ec11afedef1
coelhudo <coelhudo@gmail.com>
2015-01-28 00:44:20 -0500

    Create module, counter fixed

--- InversionCouting.hs
+++ InversionCouting.hs
@@ -1,15 +1,19 @@
+module InversionCounter (count) where
+
+import Data.List
+
 merge :: (Ord a,  Num c) => ([a], [a], c) -> ([a], c)
 merge ([x], [y], ic) | x <= y = (x : y : [],  ic)
-                   | otherwise = (y : x : [],  ic + 1)
+                     | otherwise = (y : x : [],  ic + 1)
 merge (xs, [], ic) = (xs, ic)
 merge ([], ys, ic) = (ys, ic)
-merge ((x:xs), (y:ys), ic) | x <= y = (x : fst leftMerge, (snd leftMerge))
-                           | otherwise = (y : fst rightMerge, (snd rightMerge) + 1)
+merge ((x:xs), (y:ys), ic) | x <= y = (x : fst leftMerge, snd leftMerge)
+                           | otherwise = (y : fst rightMerge, snd rightMerge)
                            where leftMerge = (merge (xs, (y:ys), ic))
                                  rightMerge = (merge ((x:xs), ys, ic))
 
-f :: (Num c) => ([a], c) -> ([a], c) -> ([a], [a], c)
-f (xs, lic) (ys, ric) = (xs, ys, lic + ric)
+f :: (Ord a, Num c) => ([a], c) -> ([a], c) -> ([a], [a], c)
+f (xs, lic) (ys, ric) = (xs, ys, lic + ric + (genericLength [(x,y) | x <- xs, y <- ys, x > y]))
 
 mergesort :: (Ord a, Num c) => ([a], c) -> ([a], c)
 mergesort (xs, ic) = merge (if len > 2 then (f (mergesort (left, ic)) (mergesort (right, ic))) else (left, right, ic))
@@ -18,6 +22,6 @@ mergesort (xs, ic) = merge (if len > 2 then (f (mergesort (left, ic)) (mergesort
                      left = fst splitted
                      right = snd splitted
 
-inversionCount :: (Ord a, Num c) => [a] -> c
-inversionCount xs = snd $ mergesort (xs, 0)
+count :: (Ord a, Num c) => [a] -> c
+count xs = snd $ mergesort (xs, 0)
 
