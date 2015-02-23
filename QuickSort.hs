eb4d4adda450bd14ed323480468f021e5a688021
coelhudo <coelhudo@gmail.com>
2015-02-23 15:35:22 -0600

    Partition done I guess

--- QuickSort.hs
+++ QuickSort.hs
@@ -2,9 +2,9 @@ import Data.List
 
 qspartition :: (Ord a) => [a] -> Int -> Int -> [a]
 qspartition [] _ _ = []
-qspartition xs i j | j == genericLength xs = xs -- need to change pivot place
-                       | xs!!j < xs!!0 = qspartition (swapElts i j xs) (i+1) (j+1)
-                       | xs!!j > xs!!0 = qspartition xs i (j+1)
+qspartition xs i j | j == genericLength xs = (swapElts (i-1) 0 xs) -- need to change pivot place
+                       | xs!!j < head xs = qspartition (swapElts i j xs) (i+1) (j+1)
+                       | xs!!j > head xs = qspartition xs i (j+1)
 
 -- https://gist.github.com/ijt/2010183
 swapElts i j ls = [get k x | (k, x) <- zip [0..length ls - 1] ls]
