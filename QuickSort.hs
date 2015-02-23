0e6759a82593e1bff71426f034078756efe3d32c
coelhudo <coelhudo@gmail.com>
2015-02-23 15:52:15 -0600

    Quicksort done

--- QuickSort.hs
+++ QuickSort.hs
@@ -1,10 +1,22 @@
 import Data.List
 
-qspartition :: (Ord a) => [a] -> Int -> Int -> [a]
-qspartition [] _ _ = []
-qspartition xs i j | j == genericLength xs = (swapElts (i-1) 0 xs) -- need to change pivot place
-                       | xs!!j < head xs = qspartition (swapElts i j xs) (i+1) (j+1)
-                       | xs!!j > head xs = qspartition xs i (j+1)
+qspart :: (Ord a) => [a] -> Int -> Int -> ([a],Int)
+qspart [] _ _ = ([],0)
+qspart xs i j | j == genericLength xs = ((swapElts (i-1) 0 xs),(i-1)) -- need to change pivot place
+              | xs!!j < head xs = qspart (swapElts i j xs) (i+1) (j+1)
+              | xs!!j > head xs = qspart xs i (j+1)
+
+qspartition :: (Ord a) => [a] -> ([a],Int)
+qspartition xs = qspart xs 1 1
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort [a] = [a]
+quicksort xs = quicksort leftSide ++ [pivot] ++ quicksort rightSide
+            where leftSide = take pivotPosition partitioned
+                  rightSide = drop (pivotPosition+1) partitioned
+                  pivot = partitioned !! pivotPosition
+                  (partitioned,pivotPosition) = qspartition xs
 
 -- https://gist.github.com/ijt/2010183
 swapElts i j ls = [get k x | (k, x) <- zip [0..length ls - 1] ls]
